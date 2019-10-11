acb628f5e4319505e51e80412c78cdb43cf97cfc
Benjamin O. M. Hansen <bomh19@student.aau.dk>
2019-10-11 09:25:05 +0200

    Updated the circle code

--- src/Hello.cpp
+++ src/Hello.cpp
@@ -1,7 +1,28 @@
-#include <iostream>
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/TeleportAbsolute.h"
+#include "turtlesim/TeleportRelative.h"
 
-int main(int argc, char const *argv[])
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "lecture2_move_turtle");
+
+  ros::NodeHandle nh;
+  ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+
+  geometry_msgs::Twist msg;
+  msg.linear.x = 1.0;
+  msg.angular.z = 0.0;
+
+
+  while(ros::ok() )
+  {
+    msg.linear.x = 1.0;
+    msg.angular.z = 1.0;
+            
+    cmd_vel_pub.publish(msg);
+    ros::Duration(1.5).sleep();
+  }
+
+  return 0;
 }
